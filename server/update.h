1611011c74e825f221afc88e767a69ca9ba22977
buj <bujtuclam@gmail.com>
2016-03-09 14:05:10 +0100

    + bugfix vysielania invazii

--- server/update.h
+++ server/update.h
@@ -5,7 +5,7 @@
 
 // o temnych rytieroch
 #define CAS_SMRT 60 // sekund
-#define CAS_TEMNOTA 30 // sekund
+#define CAS_TEMNOTA 90 // sekund
 #define POVODNA_PERIODA_TEMNYCH 50 // tahov
 #define POMALOST_TEMNYCH 100 // tahov
 #define JEDNOTIEK_TEMNYCH 50
